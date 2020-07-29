334ff9195fc47df6c912994d7eb50a077fe7bf06
Whitney Duarte <misswhitneyx@gmail.com>
2020-07-29 15:46:48 -0700

    Add project files.

---
+++ Embroidery/CanvasPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Embroidery
+{
+    /// <summary>
+    /// Interaction logic for CanvasPage.xaml
+    /// </summary>
+    public partial class CanvasPage : Page
+    {
+        Line newLine;
+        PointCollection myPointCollection = new PointCollection();
+        public CanvasPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Line_Type_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Start_Point(object sender, MouseButtonEventArgs e)
+        {
+            if (myCanvas.CaptureMouse())
+            {
+                Point position = Mouse.GetPosition(myCanvas);
+                newLine = new Line();
+                newLine.X1 = position.X;
+                newLine.X2 = position.X;
+                newLine.Y1 = position.Y;
+                newLine.Y2 = position.Y;
+                newLine.StrokeThickness = 2;
+                newLine.Stroke = System.Windows.Media.Brushes.Black;
+                myCanvas.Children.Add(newLine);
+
+            }
+
+        }
+
+        private void End_Point(object sender, MouseButtonEventArgs e)
+        {
+            ((Canvas)sender).ReleaseMouseCapture();
+        }
+
+
+
+        private void Move_Line(object sender, MouseEventArgs e)
+        {
+            if(myCanvas.IsMouseCaptured && e.LeftButton == MouseButtonState.Pressed)
+            {
+                var line = myCanvas.Children.OfType<Line>().LastOrDefault();
+                if(line != null)
+                {
+                    var endPoint = e.GetPosition(myCanvas);
+                    line.X2 = endPoint.X;
+                    line.Y2 = endPoint.Y;
+                }
+            }
+        }
+    }
+
+}
---
+++ Embroidery/SetupPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Embroidery
+{
+    /// <summary>
+    /// Interaction logic for SetupPage.xaml
+    /// </summary>
+    public partial class SetupPage : Page
+    {
+        public SetupPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            CanvasPage canvasPage = new CanvasPage();
+            this.NavigationService.Navigate(canvasPage);
+        }
+
+    
+    }
+}
